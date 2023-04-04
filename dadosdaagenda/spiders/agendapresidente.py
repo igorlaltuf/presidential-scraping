e99db677ae8cc3648868c3ffe140f1828da01b85
Igor Laltuf <igorlaltuf@gmail.com>
2023-04-04 18:44:33 -0300

    commit inicial

---
+++ dadosdaagenda/spiders/agendapresidente.py
@@ -0,0 +1,66 @@
+# Webscraping da agenda do Lula com a biblioteca Scrapy (páginas estáticas)
+
+# importar bibliotecas
+import scrapy
+import pandas as pd
+import os
+import datetime as dt
+import logging
+
+logging.basicConfig(filename='app.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(message)s', 
+                    datefmt='%d-%b-%y %H:%M:%S')
+
+class AgendaSpider(scrapy.Spider):
+    name = "agenda"
+    
+    # Automatizar downloads de 01-01-2023 até o dia de hoje
+          
+    start_date = dt.date(2023, 1, 1) 
+    end_date = dt.date.today()  
+    delta = dt.timedelta(days = 1)
+    days = []  
+    
+    
+    while start_date <= end_date:
+        days.append(start_date.strftime('%Y-%m-%d'))
+        start_date += delta
+
+            
+    def start_requests(self):
+        for day in self.days:
+             url = f'https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/agenda-do-presidente-da-republica-lula/agenda-do-presidente-da-republica/{day}'
+             yield scrapy.Request(url=url, callback=self.parse)   
+                   
+    # raspar os dados  
+    def parse(self, response):
+                        
+        autoridade = response.css('h1.documentFirstHeading::text').extract_first()       
+                        
+        titulo = response.css('h2.compromisso-titulo::text').extract()
+            
+        local = response.css('div.compromisso-local::text').extract()
+            
+        horario = response.css('time.compromisso-inicio::text').extract()
+            
+        # print(f'Título: {titulo} - Local: {local} - Horário: {horario}- Autoridade: {autoridade}')
+            
+        # criar um dataframe com os dados 
+        df = pd.DataFrame({'titulo':titulo, 'local':local, 'horario':horario})
+            
+        # inserir coluna com a data    
+        df['data'] = response.url.split('/')[-1]
+        
+        # criar coluna com o cargo da autoridade
+        df['autoridade'] = autoridade
+            
+        # criar o df que vai armazenar os dados
+        df_final = pd.DataFrame(columns=['titulo', 'local', 'horario', 'data', 'autoridade'])    
+                     
+        df_final = pd.concat([df_final, df], ignore_index=True)
+                                
+        #print(df_final)
+        
+        # exportar no formato csv    
+        df_final.to_csv('dadosdaagenda/output/compromissos.csv', sep = ';', mode='a', index=False, header=not os.path.exists('dadosdaagenda/output/compromissos.csv'))
+                
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Webscraping da agenda presidencial com a biblioteca Scrapy
+
+Este repositório é um projeto pessoal em Python que tem como objetivo criar um arquivo csv com os dados da agenda presidencial desde janeiro de 2023 e disponibilizada em: https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/agenda-do-presidente-da-republica-lula/agenda-do-presidente-da-republica/2023-03-29.
+
+
+## Requisitos
+Git 
+
+Python >= 3.10.2
+
+Virtualenv >= 20.13.0
+
+## Instalação
+
+1 - Baixe o repositório
+```
+git clone git@github.com:Clyff/nomedorepositorio.git
+$ cd nomedorepositorio
+```
+
+2 - Crie um ambiente virtual e o ative
+```
+virtualenv env
+source env/Scripts/Activate
+```
+
+3 - No ambiente, instale os pacotes
+```
+pip install -r requirements.txt
+```
+
+## Forma de usar
+
+1) No terminal, abrir a pasta "presidential-scraping".
+
+2) Execute o comando "env/Scripts/Activate" para carregar o ambiente (caso ainda não tenha feito no passo anterior). 
+
+3) Execute o comando "scrapy crawl agenda" para iniciar o web scraping.
+
+4) Execute o comando "deactivate" para sair do ambiente.
+
+Agora basta acessar o arquivo csv que dentro da pasta "output".
